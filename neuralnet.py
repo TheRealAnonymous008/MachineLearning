af3004cb33918cc770569c13454fd6604b4bb01f
Joaquin dela Rosa <62501229+TheRealAnonymous008@users.noreply.github.com>
2023-09-29 20:05:36 +0800

    feat: add crude neural net model

---
+++ neuralnet.py
@@ -0,0 +1,42 @@
+import torch.functional as F
+import torch.nn as nn
+import torch
+
+class NeuralNetwork(nn.Module):
+    def __init__(self, input_features, hidden_layers, output_classes, dropout = True):
+        super().__init__()
+
+        self.initialize_layers(input_features, hidden_layers, output_classes, dropout)
+
+        
+    def initialize_layers(self, input_features, hidden_layers, output_classes, dropout):
+        # Make it easier to create the liear layers
+        dimensions = [input_features]
+        dimensions.extend(hidden_layers)
+        dimensions.append(output_classes)
+
+        self.forward_layers = []
+
+        for x in range(1, len(dimensions)):
+            self.forward_layers.append(
+                nn.Linear(dimensions[x - 1], dimensions[x]),
+            )
+
+            if x < len(dimensions) - 1: 
+                self.forward_layers.append(nn.ReLU())
+
+                if dropout: 
+                    self.forward_layers.append(nn.Dropout())
+            
+            else:
+                self.forward_layers.append(nn.Sigmoid())
+
+
+
+    def forward(self, x):
+        output = x 
+        
+        for layer in self.forward_layers:
+            output = layer.forward(x)
+        
+        return x
