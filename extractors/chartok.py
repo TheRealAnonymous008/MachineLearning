87cf20dd3acf7365a6f9f2fe9c3858ca0d2a6de5
TheRealAnonymous008 <joaquinantoniodelarosa@gmail.com>
2023-10-28 17:34:29 +0800

    feat: char level tokenizer now outputs number vectors instead of char vectors

--- extractors/chartok.py
+++ extractors/chartok.py
@@ -8,7 +8,9 @@ class CharacterTok:
         max_length = text.str.len().max()
         
         # Apply tokenization and padding
-        tokenized_series = text.apply(lambda text: list(text.ljust(max_length, " ")))
+        char_to_int = {char: i for i, char in enumerate(' abcdefghijklmnopqrstuvwxyz')}
+        tokenized_series = text.apply(lambda text: [char_to_int[char] for char in text.ljust(max_length, self.pad_token)])
+        
         
         # Create a DataFrame from the tokenized Series
         token_matrix = pd.DataFrame(list(tokenized_series))
