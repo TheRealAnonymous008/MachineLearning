daed601cfd185ecfc2659f98317d1659e115df3e
TheRealAnonymous008 <joaquinantoniodelarosa@gmail.com>
2023-11-02 19:05:22 +0800

    feat: transformers

--- utils/transformer.py
+++ utils/transformer.py
@@ -3,26 +3,29 @@ import torch.nn as nn
 import torch
 
 class TransformerEncoder(nn.Module):
-    def __init__(self, input_size, d_model, num_heads, num_layers, dropout=0.1):
-        super(TransformerEncoder, self).__init__()
+    def __init__(self, input_size, output_size, embed_dims, num_heads, num_layers, dropout=0.1):
+        super().__init__()
 
         # Embedding layer to convert input data into d_model-dimensional vectors
-        self.embedding = nn.Linear(input_size, d_model)
+        self.embedding = nn.Embedding(255, embed_dims, ord("\n"))
 
         # Transformer Encoder layers
         self.encoder = nn.TransformerEncoder(
             nn.TransformerEncoderLayer(
-                d_model=d_model,
+                d_model=embed_dims,
                 nhead=num_heads,
-                dim_feedforward=4 * d_model,
+                dim_feedforward= 4 * embed_dims,
                 dropout=dropout
             ),
             num_layers=num_layers
         )
+        
+        self.fc = nn.Linear(embed_dims, output_size)
 
     def forward(self, x):
-        x = x.permute(1, 0, 2) 
         embedded = self.embedding(x)
         encoded = self.encoder(embedded)
         encoded = encoded.permute(1, 0, 2) 
-        return encoded
+
+        y = self.fc(encoded[-1, :, :])
+        return y
