bbba63424ad3dc6585d2d5ebe87d6ac15abad027
TheRealAnonymous008 <joaquinantoniodelarosa@gmail.com>
2023-11-04 20:06:57 +0800

    feat: positional embedding for xformer

--- utils/transformer.py
+++ utils/transformer.py
@@ -1,20 +1,46 @@
 import torch.functional as F
 import torch.nn as nn
 import torch
+import math
 
+class PositionalEncoding(nn.Module):
+    def __init__(self, d_model, max_seq_length=512, device = "cuda"):
+        super(PositionalEncoding, self).__init__()
+        self.dropout = nn.Dropout(p=0.1)
+        
+        # Create positional embeddings
+        position = torch.arange(0, max_seq_length).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2) * -(math.log(10000.0) / d_model))
+        
+        pos_embed = torch.zeros(1, max_seq_length, d_model)
+        pos_embed[0, :, 0::2] = torch.sin(position * div_term)
+        pos_embed[0, :, 1::2] = torch.cos(position * div_term)
+        pos_embed = pos_embed.to(device)
+
+        self.pos_embed = pos_embed
+        
+        self.device = device
+
+    def forward(self, x):
+        x = x + self.pos_embed
+        return self.dropout(x)
+    
 class TransformerEncoder(nn.Module):
-    def __init__(self, input_size, output_size, embed_dims, num_heads, num_layers, dropout=0.1, device = "cuda"):
+    def __init__(self, input_size, output_size, embed_dims, num_heads, num_layers, ff = 10, dropout=0.1, device = "cuda"):
         super().__init__()
 
         # Embedding layer to convert input data into d_model-dimensional vectors
         self.embedding = nn.Embedding(255, embed_dims, ord("\n"))
+
+        # Positional encoding
+        self.positional_encoding = PositionalEncoding(embed_dims, input_size)
 
         # Transformer Encoder layers
         self.encoder = nn.TransformerEncoder(
             nn.TransformerEncoderLayer(
                 d_model=embed_dims,
                 nhead=num_heads,
-                dim_feedforward= 4 * embed_dims,
+                dim_feedforward= ff,
                 dropout=dropout
             ),
             num_layers=num_layers
@@ -26,7 +52,8 @@ class TransformerEncoder(nn.Module):
 
     def forward(self, x):
         x = x.to(self.device)
-        embedded = self.embedding(x)
+        embedded = self.embedding(x) 
+        embedded = self.positional_encoding(embedded)
         encoded = self.encoder(embedded)
         encoded = encoded.permute(1, 0, 2) 
 
