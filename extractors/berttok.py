254b1af6bd1ec4ae0dc10ac0a2956539cdffd7f1
TheRealAnonymous008 <joaquinantoniodelarosa@gmail.com>
2023-11-15 08:37:48 +0800

    feat: bert encodings

---
+++ extractors/berttok.py
@@ -0,0 +1,22 @@
+
+
+from transformers import AutoTokenizer, AutoModelForMaskedLM
+import pandas as pd 
+import numpy as np 
+
+class BertTokenizer: 
+    tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
+    model = AutoModelForMaskedLM.from_pretrained("bert-base-uncased")
+    
+    def __init__(self):
+        pass
+
+    def encode(self, sentences : pd.Series): 
+        encoded_outputs =  []
+        for sentence in sentences: 
+            inputs = self.tokenizer.encode(sentence, return_tensors="pt")
+            outputs = self.model(inputs)
+            output = outputs.logits[:, -1, :].view(-1)
+            encoded_outputs.append(output)
+
+        return np.array(encoded_outputs)
